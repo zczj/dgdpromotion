17acf051289e3b2ed56cfcc64e40470167f39295
suzhihui <wisdom.su@163.com>
2016-11-29 18:32:25 +0800

    add files

--- src/skin/promotion/standBao/js/pro.js
+++ src/skin/promotion/standBao/js/pro.js
@@ -1,1 +1,104 @@
-pro.js
+/*
+* @Author: suzhihui
+* @Date:   2016-07-19 09:32:40
+* @Last Modified by:   老苏
+* @Last Modified time: 2016-11-29 18:25:35
+*/
+
+$(window).on('scroll', function () {
+    $('.mark').height($(document).height())
+
+});
+$('.buy').click(function () {
+    if ($(this).hasClass('no-hoved')) {
+        return;
+    }
+    var userid = $("#UserID").val();
+    $.post("/Special/checkLogin", function (data) {
+
+        if (data.state == "noLogin") {
+            $.layer({
+                shade: [0],
+                area: ['200', 'auto'],
+                dialog: {
+                    msg: '请先登录',
+                    btns: 2,
+                    type: 4,
+                    btn: ['确认', '取消'],
+                    yes: function () {
+                        location.href = "/passport/login?rel=" + $('#requestUrl').val();
+                    }, no: function () {
+                        return;
+                    }
+                }
+            });
+            return;
+        }
+        else if (data.state == "timeout") {
+            layer.alert(data.msg);
+            return;
+
+        }
+
+        else if (data.state == "nobuy") {
+            $(".buy").html("10:00更新金额");
+            layer.alert(data.msg, 8);
+            return;
+        }
+        else if (data.state== "noauth")
+        {
+            $.layer({
+                shade: [0],
+                area: ['200', 'auto'],
+                dialog: {
+                    msg: '请先认证投资人',
+                    btns: 2,
+                    type: 4,
+                    btn: ['确认','取消'],
+                    yes: function () {
+                        location.href = "/mycenter/authentication?rel=" + $('#requestUrl').val();
+                    }, no: function () {
+                        return;
+                    }
+                }
+            });
+
+        }
+        else {
+
+            popFn($('#popBox'), $('#mark'));
+        }
+
+    });
+
+});
+$('#mark,#popBox .close').click(function () {
+    hideFn($('#popBox'),$('#mark'));
+})
+
+function popFn(obj,mark) {
+    mark.fadeIn(30, function () {
+        obj.fadeIn();
+    })
+}
+function hideFn(obj,mark) {
+    obj.fadeOut(30, function () {
+        mark.fadeOut();
+    })
+}
+// $(function () {
+//     if (browserRedirect()) {
+//         $('.tophead-warp,.layer-box').remove();
+//     }
+// })
+
+//自定义弹窗
+/*
+@parma tips :提示语
+@parma fn : 回调函数
+*/
+function myAlert(tips, fn) {
+    popFn($('#myAlert'), $('#mark1'));
+    $('.popBox-myalert .myalert-tips').html(tips);
+    fn && fn();
+}
